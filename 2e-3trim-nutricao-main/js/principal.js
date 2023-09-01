67aaaae17011c9abc12155a4fcce39f85a1ea058
wagnermigue <129407747+wagnermigue@users.noreply.github.com>
2023-09-01 09:33:19 -0300

    Add files via upload

---
+++ 2e-3trim-nutricao-main/js/principal.js
@@ -0,0 +1,11 @@
+let titulo = document.querySelector ("h1").textContent;
+
+console.log(titulo);
+
+titulo.textContent = "fica grandao bill"
+
+
+//-------aqui vamos mexer na tabela de imc----
+
+let paciente = document.querySelector("#primeiro-paciente");
+console.log(paciente);
